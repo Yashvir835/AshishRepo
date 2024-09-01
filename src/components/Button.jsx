28015cca970d6a7b6ba325024417f85debaa7237
https://github.com/Yashvir835/Prakartik-Ahaar <singhyashvir835@gmail.com>
2024-09-01 23:15:22 +0530

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../styles/button1.css'
+const Button = () => {
+  return (
+    <>
+<div className='heading'>
+<h3>This contain confedential informatiion . So choose when you are alone </h3>
+<h1>Choose Any one </h1>
+  </div>      
+
+    <div className='wrapper'>
+   
+       <a href="https://yashvir835.github.io/Ashish/">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Click
+      </a>
+      <a href="https://yashvir835.github.io/Ashish/">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Click
+      </a>
+      <a href="https://yashvir835.github.io/Ashish/">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Click
+      </a>
+    </div>
+    </>
+  )
+}
+
+export default Button;
